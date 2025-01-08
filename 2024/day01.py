6a0b6e5c5d88f17324fccb31afa3b37e10faea4f
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2025-01-08 14:19:46 +0100

    refactor

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,17 +1,6 @@
 import re
-from pathlib import Path
 
-
-def get_abs_path(filename: str) -> Path:
-    return Path(__file__).parent / filename
-
-
-def get_data(filename: str) -> str:
-    return get_abs_path(filename).read_text()
-
-
-def get_lines(filename: str) -> list[str]:
-    return get_data(filename).splitlines()
+from utils import get_lines
 
 
 def read_lists(filename: str) -> tuple[list[int], list[int]]:
